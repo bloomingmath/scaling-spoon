2729412c7786b3ade72e408a81cc5bc3f15a67a3
Matteo Bortolotto <yamatteo@gmail.com>
2019-12-19 17:51:38 -0300

    Simpler tests

--- tests/test_a.py
+++ tests/test_a.py
@@ -6,61 +6,11 @@ from starlette.testclient import TestClient
 from starlette.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 import fastapi
+from importlib import import_module
 
 
-def fake_module(name: str):
-    from types import ModuleType
 
-    m = ModuleType(name)
-
-    class User:
-        username = Required(str, unique=True)
-        email = Required(str, unique=True)
-        salt = Required(str)
-        hashed = Required(str)
-        fullname = Optional(str)
-
-        def create_preparation(self, username: str, email: str, password: str, fullname: str = None):
-            salt = helpers.encryption.generate_salt()
-            hashed = helpers.encryption.hash_password(salt, password)
-            create_info = {"username": username, "email": email, "salt": salt, "hashed": hashed}
-            if fullname is not None:
-                create_info["fullname"] = fullname
-            return create_info
-
-        def get_preparation(self, id: int = None, username: str = None, email: str = None):
-            get_info = {}
-            if id is not None:
-                get_info["id"] = id
-            if username is not None:
-                get_info["username"] = username
-            if email is not None:
-                get_info["email"] = email
-            return get_info
-
-        def select_preparation(self):
-            select_info = {}
-            return select_info
-
-        def update_preparation(self, password: str = None, fullname: str = None):
-            update_info = {}
-            if password is not None:
-                salt = self.salt
-                hashed = helpers.encryption.hash_password(salt, password)
-                update_info["hashed"] = hashed
-            if fullname is not None:
-                update_info["fullname"] = fullname
-            return update_info
-
-        def authenticate(self, password: str):
-            return helpers.encryption.verify_password(self.salt, self.hashed, password)
-
-    User.__module__ = name
-    m.User = User
-    return m
-
-
-bases = fake_module("bases")
+bases = import_module("bases")
 
 mc = ModelContainer(bases, provider="sqlite", filename=":memory:", create_db=True)
 
@@ -74,4 +24,8 @@ app.include_router(stage_a.make_router(mc, app, templates))
 app.add_middleware(SessionMiddleware, secret_key=helpers.encryption.generate_salt())
 
 client = TestClient(app)
+
+def test_app_is_online():
+    response = client.get("/")
+    assert response.status_code == 200
 
