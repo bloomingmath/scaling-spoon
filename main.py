41b5cea50cdf1dfc11c63e2ebf80ba2ba85631cb
Matteo Bortolotto <yamatteo@gmail.com>
2019-12-20 16:19:56 -0300

    Middleware to detect 'next' url query parameter in every request and inject it as location header in eventual Http303 response.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from popy import ModelContainer  # , db_session, Required, Optional
-import helpers.encryption
-from routers import stage_a
-from starlette.templating import Jinja2Templates
-from starlette.staticfiles import StaticFiles
-from starlette.middleware.sessions import SessionMiddleware
+from blinker import signal
 from fastapi import FastAPI
 from importlib import import_module
-from blinker import signal
-from helpers import load_flashes
+from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+from helpers import generate_salt
+from helpers import load_flashes
+from popy import ModelContainer
+from routers import stage_a
 
 # Import bases (pony-like classes) and generate database, database's models, schemas and operations
 bases = import_module("bases")
@@ -21,10 +22,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(stage_a.make_router(mc, app, templates))
 
 # Adding middlewares
-app.add_middleware(SessionMiddleware, secret_key=helpers.encryption.generate_salt())
+app.add_middleware(SessionMiddleware, secret_key=generate_salt())
+
 
 @app.middleware("http")
 async def next_url_redirect(request: Request, call_next):
+    """Check if 'next' query parameter is present in the request. If so, inject it as next url in an eventual Http303
+    response. """
     try:
         url = request.query_params["next"]
         assert isinstance(url, str)
@@ -37,6 +41,7 @@ async def next_url_redirect(request: Request, call_next):
     if url is not None and response.status_code == 303:
         response.headers["location"] = url
     return response
+
 
 # Set up blinker signaling system
 app.signals = {
--- middlewares.py
+++
@@ -1,8 +0,0 @@
-from starlette.middleware.base import BaseHTTPMiddleware
-
-class RedirectNextMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request, call_next):
-        next = request.query_params.get("next", None)
-        response = await call_next(request)
-        response.headers['X-Custom'] = next
-        return response
